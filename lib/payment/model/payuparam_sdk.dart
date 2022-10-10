e13f8d60fa5dbdb28728ef094d8fcac89e515176
20anirudh20 <kppkanu@gmail.com>
2022-10-10 23:50:46 +0530

    Major change

--- lib/payment/model/payuparam_sdk.dart
+++ lib/payment/model/payuparam_sdk.dart
@@ -1,72 +1,76 @@
 class PayuParamSdk {
-  String? key;
-  String? payUTxnId;
   String? amount;
-  String? originalAmount;
   String? commissionAmount;
-  String? productInfo;
-  String? firstName;
   String? email;
+  String? firstName;
   String? hashed;
+  String? key;
+  String? originalAmount;
+  String? payUTxnId;
+  String? paymentMode;
+  String? productInfo;
   String? udf1;
   String? udf2;
   String? udf3;
   String? udf4;
   String? udf5;
-  String? paymentMode;
+  String? voucherId;
 
   PayuParamSdk(
-      {this.key,
-      this.payUTxnId,
-      this.amount,
-      this.originalAmount,
+      {this.amount,
       this.commissionAmount,
-      this.productInfo,
-      this.firstName,
       this.email,
+      this.firstName,
       this.hashed,
+      this.key,
+      this.originalAmount,
+      this.payUTxnId,
+      this.paymentMode,
+      this.productInfo,
       this.udf1,
       this.udf2,
       this.udf3,
       this.udf4,
       this.udf5,
-      this.paymentMode});
+      this.voucherId});
 
   PayuParamSdk.fromJson(Map<String, dynamic> json) {
-    key = json['key'];
-    payUTxnId = json['payUTxnId'];
     amount = json['amount'];
-    originalAmount = json['originalAmount'];
     commissionAmount = json['commissionAmount'];
-    productInfo = json['productInfo'];
-    firstName = json['firstName'];
     email = json['email'];
+    firstName = json['firstName'];
     hashed = json['hashed'];
+    key = json['key'];
+    originalAmount = json['originalAmount'];
+    payUTxnId = json['payUTxnId'];
+    paymentMode = json['paymentMode'];
+    productInfo = json['productInfo'];
     udf1 = json['udf1'];
     udf2 = json['udf2'];
     udf3 = json['udf3'];
     udf4 = json['udf4'];
     udf5 = json['udf5'];
-    paymentMode = json['paymentMode'];
+    voucherId = json['voucherId'];
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['key'] = this.key;
-    data['payUTxnId'] = this.payUTxnId;
     data['amount'] = this.amount;
-    data['originalAmount'] = this.originalAmount;
     data['commissionAmount'] = this.commissionAmount;
-    data['productInfo'] = this.productInfo;
-    data['firstName'] = this.firstName;
     data['email'] = this.email;
+    data['firstName'] = this.firstName;
     data['hashed'] = this.hashed;
+    data['key'] = this.key;
+    data['originalAmount'] = this.originalAmount;
+    data['payUTxnId'] = this.payUTxnId;
+    data['paymentMode'] = this.paymentMode;
+    data['productInfo'] = this.productInfo;
     data['udf1'] = this.udf1;
     data['udf2'] = this.udf2;
     data['udf3'] = this.udf3;
     data['udf4'] = this.udf4;
     data['udf5'] = this.udf5;
-    data['paymentMode'] = this.paymentMode;
+    data['voucherId'] = this.voucherId;
     return data;
   }
 }
